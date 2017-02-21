45e7bdc611e63bf1ffb8465a480f454babcbd5b7
陳柏亨 <PoHeng.Chen@gmail.com>
2017-02-21 15:27:20 -0500

    1. finished organization list crawler 2. rename the file

--- crawlOrgList.py
+++ crawlOrgList.py
@@ -1,5 +1,6 @@
 import urllib
 from bs4 import BeautifulSoup as bs
+import re
 
 # find all states
 url_base = 'http://greatnonprofits.org'
@@ -18,4 +19,19 @@ for ul in uls:
         state_list.append((tmp.text, tmp['href']))
         
 
-#
+# walking through each state
+state_list[0][1]
+url_page = '/sort:review_count/direction:desc/page:'
+
+page = urllib.urlopen(url_base + state_list[0][1] + url_page)
+soup = bs(page)
+
+# get organization list
+org_list = []
+ol = soup.find('ol', {'class', 'gnp-searchResults'})
+ol = ol.find_all('div', {'class', 'gnp-searchResult-infoMajor'})
+for i in ol:
+    org_name = re.sub('\n','',tmp.text).strip()
+    org_url = i['href']
+    org_list.append((org_name, org_url))
+    
