690421985d278c61fd329b61bacdd3002e0e5988
陳柏亨 <PoHeng.Chen@gmail.com>
2017-02-21 15:48:49 -0500

    1. finished organization list crawler

--- crawlOrgList.py
+++ crawlOrgList.py
@@ -18,20 +18,35 @@ for ul in uls:
         tmp = li.find('a')
         state_list.append((tmp.text, tmp['href']))
         
-
 # walking through each state
-state_list[0][1]
-url_page = '/sort:review_count/direction:desc/page:'
+for state in state_list:
+    state_name = state[0]
+    state_url = state[1]
+    print 'Start walking through: %s' % state_name 
 
-page = urllib.urlopen(url_base + state_list[0][1] + url_page)
-soup = bs(page)
+    # walking through each page:
+    org_list = []
+    for i in xrange(1,51):
+        url_page = '/sort:review_count/direction:desc/page:'
+        page = urllib.urlopen(url_base + state_url + url_page + str(i))
+        soup = bs(page)
 
-# get organization list
-org_list = []
-ol = soup.find('ol', {'class', 'gnp-searchResults'})
-ol = ol.find_all('div', {'class', 'gnp-searchResult-infoMajor'})
-for i in ol:
-    org_name = re.sub('\n','',tmp.text).strip()
-    org_url = i['href']
-    org_list.append((org_name, org_url))
-    
+        # get organization list
+        ol = soup.find('ol', {'class', 'gnp-searchResults'})
+        ol = ol.find_all('div', {'class', 'gnp-searchResult-infoMajor'})
+        for org in ol:
+            org = org.a
+            org_name = re.sub('\n','',org.text).strip()
+            org_url = org['href']
+            org_list.append((state_name, state_url, org_name, org_url))
+        print '%s: Page %s of 50 is done!' % (state_name, i) 
+
+    # write down data by state
+    file_name = state_name + '.tsv'
+    print 'Start writing %s' % state_name
+    with open(file_name, "wb") as writer:
+        writer.write('state_name\tstate_url\torg_name\torg_url\n')
+        for state_name, state_url, org_name, org_url in org_list:
+            writer.write('{0}\t{1}\t{2}\t{3}\n'.format(state_name, state_url, org_name, org_url))
+    print '%s is done!' % state_name
+
