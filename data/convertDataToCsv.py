b5781eb16f80f3c27e8deecc67329c16074f8160
陳柏亨 <PoHeng.Chen@gmail.com>
2017-03-03 16:32:24 -0500

    1. finalized convertDataToCsv.py

--- data/convertDataToCsv.py
+++ data/convertDataToCsv.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 ###### organization data
 # read in all organization data
-dir_base = 'orgData/'
+dir_base = 'data/orgData/'
 files = os.listdir(dir_base)
 
 org_data = pd.DataFrame()
@@ -12,12 +12,15 @@ for file_dir in files:
     # read each file and go through data 
     with open(dir_base + file_dir, 'rb') as tsv:
         file = tsv.readlines()
+    state = file_dir[:-4]
     d = [row.replace('\n','').split('\t') for row in file]
     d = pd.DataFrame(d[1: ], columns = d[0])
-    d = d[['name', 'url', 'telephone', 'tax_num', 'street', 'locality', 'region', 'postcode', 'country']]
+    d['state'] = state
+    d = d[['name', 'url', 'telephone', 'tax_num', 'street', 'locality', 'state', 'region', 'postcode', 'country']]
     org_data = org_data.append(d, ignore_index=True)
+    print '%s is done.' % state
 
-org_data.to_csv('orgData.csv', index = False)
+org_data.to_csv('data/orgData.csv', index = False)
 
 
 
@@ -31,8 +34,11 @@ for file_dir in files:
     # read each file and go through data 
     with open(dir_base + file_dir, 'rb') as tsv:
         file = tsv.readlines()
+    state = file_dir[:-4]
     d = [row.replace('\n','').split('\t') for row in file]
     d = pd.DataFrame(d[1: ], columns = d[0])
+    d['state'] = state
     org_review = org_review.append(d, ignore_index=True)
+    print '%s is done.' % state
 
-org_review.to_csv('orgReview.csv', index = False)
+org_review.to_csv('data/orgReview.csv', index = False)
