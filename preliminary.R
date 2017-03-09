fa30dbcab44dad698d4989cb5336740a39754543
陳柏亨 <PoHeng.Chen@gmail.com>
2017-03-08 22:10:06 -0500

    1. revised codes to plot correct PDF, CDF, scatter plot of distribution

--- preliminary.R
+++ preliminary.R
@@ -1,3 +1,4 @@
+setwd('~/Documents/umsi/Ceren/greatNonprofits/')
 library(dplyr)
 library(data.table)
 library(ggplot2)
@@ -17,49 +18,77 @@ data = read.csv('data/orgReview.csv', as.is = T)
 data = data.table(data)
 d = data
 d = d %>% group_by(org) %>% summarize(num_review = n())
-d$total_review = sum(d$num_review)
-d$percent = d$num_review / d$total_review
-p = data.frame(table(d$num_review))
-p$Var1 = as.numeric(as.character(p$Var1))
-p$CDF = cumsum(p$Freq)
+p = data.table(table(d$num_review))
+names(p) = c('num_review', 'num_org')
+p$num_review = as.numeric(as.character(p$num_review))
+p$num_org_CDF = cumsum(p$num_org)
 
-# basic PDF, CDF 
+# basic scatter plot of distribution
 ggplot() +
-    geom_line(data = p, aes(x = Var1, y = Freq, col = 'PDF')) +
-    geom_line(data = p, aes(x = Var1, y = CDF, col = 'CDF')) +
+    geom_point(data = p, aes(x = num_review, y = num_org), col = 'red', size = 0.5) +
     scale_x_continuous('number of review') + 
     scale_y_continuous('number of organization') +  
-    ggtitle("Organization") +
+    ggtitle("Organization Distribution") +
     theme(plot.title = element_text(hjust = 0.5))
 ggsave(file="plot/organization.png")
 
-# PDF, CDF in log
+# basic scatter plot of distribution in log
 ggplot() +
-    geom_line(data = p, aes(x = Var1, y = Freq, col = 'PDF')) +
-    geom_line(data = p, aes(x = Var1, y = CDF, col = 'CDF')) +
+    geom_point(data = p, aes(x = num_review, y = num_org), col = 'red', size = 0.5)+
     scale_x_log10('number of review') + 
     scale_y_log10('number of organization')+  
-    ggtitle("Organization (log)") +
+    ggtitle("Organization Distribution (log)") +
     theme(plot.title = element_text(hjust = 0.5))
 ggsave(file="plot/organization_log.png")
 
+# basic PDF density
+ggplot(p, aes(num_review) ) + 
+    geom_density(alpha = 0.1, col = 'red', fill = 'red') + 
+    guides(fill = F, col = F) + # to remove legend
+    ggtitle('Organization PDF') +
+    xlab('number of review') +
+    theme(plot.title = element_text(hjust = 0.5))
+ggsave(file="plot/organization_pdf.png")
+
+# basic CDF 
+cdf = ggplot(p, aes(num_review)) + 
+    stat_ecdf(geom = "step", col = 'blue') + 
+    guides(fill = F, col = F) + # to remove legend
+    ggtitle('Organization CDF') +
+    xlab('number of review') +
+    ylab('CDF') +
+    theme(plot.title = element_text(hjust = 0.5))
+cdf
+ggsave(file="plot/organization_cdf.png")
+
+# basic CCDF
+tmp <- ggplot_build(cdf)$data[[1]]
+ggplot(tmp, aes(x = x, y = 1-y )) + 
+    geom_step(col = 'blue') +
+    guides(fill = F, col = F) + # to remove legend
+    ggtitle('Organization CCDF') +
+    xlab('number of review') +
+    ylab('CCDF') +
+    theme(plot.title = element_text(hjust = 0.5))
+ggsave(file="plot/organization_ccdf.png")
 
 # - what fraction of reviews come from a certain kind (e.g.  "General Member of the Public").
 #   Again a CDF (x axis: fraction of reviews from  "General Member of the Public", 
 #   y-axis: fraction of orgs that have that fraction for that type). We can have a curve per type.
 
 d = data
-d = d %>% select(type, org) %>% group_by(type) %>% mutate(total_review = n()) %>% 
-    group_by(type, org, total_review) %>% summarize(num_review = n()) %>% 
+d = d %>% select(org, type) %>% group_by(org) %>% mutate(total_review = n()) %>%
+    group_by(org, type, total_review) %>% summarize(num_review = n()) %>%
     mutate(fraction_review = num_review / total_review)
 
 p = list()
 for(t in unique(d$type)){
     tmp = d[which(d$type == t),]
-    tmp = data.frame(table(tmp$fraction_review), stringsAsFactors =  F)
-    tmp$Var1 = as.numeric(as.character(tmp$Var1))
-    tmp$Freq = cumsum(tmp$Freq) 
-    tmp$Percent = tmp$Freq / max(tmp$Freq)
+    tmp = data.table(table(tmp$fraction_review))
+    names(tmp) = c('fraction_review', 'num_org')
+    tmp$fraction_review = as.numeric(tmp$fraction_review)
+    tmp$fraction_org = tmp$num_org / sum(tmp$num_org)
+    tmp$cum_fraction_org = cumsum(tmp$fraction_org) 
     if (t == '') {
         tmp$type = 'NA'
     } else {
@@ -69,44 +98,43 @@ for(t in unique(d$type)){
 }
 p = do.call(rbind, p)
 
-# basic CDF
-ggplot() +
-    geom_line(data = p, aes(x = Var1, y = Freq, group = type, col = type)) + 
+## reference:
+## http://stackoverflow.com/questions/11344561/controlling-line-color-and-line-type-in-ggplot-legend
+
+# basic Cumulative distribution
+ggplot(data = p, aes(x = fraction_review, y = cum_fraction_org, group = type)) +
+    geom_line(aes(col = type, linetype = type)) + 
+    scale_linetype_manual(values = rep(c('solid', 'dashed'), 5)) + 
+    scale_color_manual(values = sort(rep(c(1:4,'grey50'), 2))) + 
     scale_x_continuous('fraction of review') + 
-    scale_y_continuous('number of orgnization') +  
-    ggtitle("CDF of Organization in given types") +
-    ggtitle("CDF of Organization in given types") +
-    theme(plot.title = element_text(hjust = 0.5))
+    scale_y_continuous('fraction of orgnization') +  
+    ggtitle("Cummulative Organization Distribution in given types") 
+    # theme(plot.title = element_text(hjust = 0.5))
+    # theme_bw() # remove grey background
 ggsave(file="plot/organization_type.png")
 
-# basic CDF long
-ggplot() +
-    geom_line(data = p, aes(x = Var1, y = Freq, group = type, col = type)) + 
+# basic Cumulative distribution logx
+ggplot(data = p, aes(x = fraction_review, y = cum_fraction_org, group = type)) +
+    geom_line(aes(col = type, linetype = type)) + 
+    scale_linetype_manual(values = rep(c('solid', 'dashed'), 5)) + 
+    scale_color_manual(values = sort(rep(c(1:4,'grey50'), 2))) + 
     scale_x_log10('fraction of review') + 
-    scale_y_log10('number of orgnization') +  
-    ggtitle("CDF of Organization in given types (log)") +
-    theme(plot.title = element_text(hjust = 0.5))
-ggsave(file="plot/organization_type_log.png")
+    scale_y_continuous('fraction of orgnization') +  
+    ggtitle("Cummulative Organization Distribution given Types (logx)") 
+    # theme(plot.title = element_text(hjust = 0.5))
+ggsave(file="plot/organization_type_logx.png")
 
+# basic Cumulative distribution logxy
+ggplot(data = p, aes(x = fraction_review, y = cum_fraction_org, group = type)) +
+    geom_line(aes(col = type, linetype = type)) + 
+    scale_linetype_manual(values = rep(c('solid', 'dashed'), 5)) + 
+    scale_color_manual(values = sort(rep(c(1:4,'grey50'), 2))) + 
+    scale_x_log10('fraction of review') + 
+    scale_y_log10('fraction of orgnization') +  
+    ggtitle("Cummulative Organization Distribution given Types (logxy)") 
+# theme(plot.title = element_text(hjust = 0.5))
+ggsave(file="plot/organization_type_logxy.png")
 
-# # Normalized CDF
-# ggplot() +
-#     geom_line(data = p, aes(x = Var1, y = Percent, group = type, col = type)) + 
-#     scale_x_continuous('fraction of review') + 
-#     scale_y_continuous('percentage') +   
-#     ggtitle("Organization in given types (normalized)") +
-#     theme(plot.title = element_text(hjust = 0.5))
-# ggsave(file="plot/organization_type_percent.png")
-# 
-# # Normalized CDF in log
-# ggplot() +
-#     geom_line(data = p, aes(x = Var1, y = Percent, group = type, col = type)) + 
-#     scale_x_log10('fraction of review') + 
-#     scale_y_continuous('percentage') +   
-#     ggtitle("Organization in given types (normalized, log)") +
-#     theme(plot.title = element_text(hjust = 0.5))
-# ggsave(file="plot/organization_type_percent_log.png")
-# 
 
 # 3) Per user:
 # - how many reviews do a user write? We can create PDF and CDF plots (x axis: number of reviews,
@@ -114,50 +142,81 @@ ggsave(file="plot/organization_type_log.png")
 
 d = data
 d = d %>% group_by(reviewer) %>% summarize(num_review = n())
-d$total_review = sum(d$num_review)
-d$percent = d$num_review / d$total_review
-p = data.frame(table(d$num_review))
-p$Var1 = as.numeric(as.character(p$Var1))
-p$CDF = cumsum(p$Freq)
+p = data.table(table(d$num_review))
+names(p) = c('num_review', 'num_reviewer')
+p$num_review = as.numeric(p$num_review)
 
-# basic PDF, CDF 
+# basic scatter plot of distribution
 ggplot() +
-    geom_line(data = p, aes(x = Var1, y = Freq, col = 'PDF')) +
-    geom_line(data = p, aes(x = Var1, y = CDF, col = 'CDF')) +
+    geom_point(data = p, aes(x = num_review, y = num_reviewer), col = 'red', size = 0.5) +
     scale_x_continuous('number of review') + 
     scale_y_continuous('number of reviewer') + 
-    ggtitle("Reviewer") +
+    ggtitle("Reviewer Distribution") +
     theme(plot.title = element_text(hjust = 0.5))
 ggsave(file="plot/reviewer.png")
 
-# PDF, CDF in log
+
+# basic scatter plot of distribution in log
 ggplot() +
-    geom_line(data = p, aes(x = Var1, y = Freq, col = 'PDF')) +
-    geom_line(data = p, aes(x = Var1, y = CDF, col = 'CDF')) +
+    geom_point(data = p, aes(x = num_review, y = num_reviewer), col = 'red', size = 0.5) +
     scale_x_log10('number of review') + 
     scale_y_log10('number of reviewer') +
-    ggtitle("Reviewer (log)") +
+    ggtitle("Reviewer Distribution (log)") +
     theme(plot.title = element_text(hjust = 0.5))
 ggsave(file="plot/reviewer_log.png")
+
+
+# basic PDF density
+ggplot(p, aes(num_review) ) + 
+    geom_density(alpha = 0.1, col = 'red', fill = 'red') + 
+    guides(fill = F, col = F) + # to remove legend
+    ggtitle('Reviewer PDF') +
+    theme(plot.title = element_text(hjust = 0.5))
+ggsave(file="plot/reviewer_pdf.png")
+
+# basic CDF 
+cdf <- ggplot(p, aes(num_review)) + 
+    stat_ecdf(geom = "step", col = 'blue') + 
+    guides(fill = F, col = F) + # to remove legend
+    ggtitle('Reviewer CDF') +
+    xlab('number of review') +
+    ylab('CDF') +
+    theme(plot.title = element_text(hjust = 0.5))
+cdf
+ggsave(file="plot/reviewer_cdf.png")
+
+
+# basic CCDF
+tmp <- ggplot_build(cdf)$data[[1]]
+ggplot(tmp, aes(x = x, y = 1-y )) + 
+    geom_step(col = 'blue') +
+    guides(fill = F, col = F) + # to remove legend
+    ggtitle('Reviewer CCDF') +
+    xlab('number of review') +
+    ylab('CCDF') +
+    theme(plot.title = element_text(hjust = 0.5))
+ggsave(file="plot/reviewer_ccdf.png")
+
 
 # - what fraction of reviews written by an individual are in a certain role (e.g. "General Member
 #   of the Public") Again a CDF (x axis: fraction of reviews written as a  "General Member of the 
 #   Public", y-axis: fraction of users that have written at least that fraction of their reviews in
 #   that role). We can have a curve per type again.
 
-d = data
 
-d = d %>% select(type, reviewer) %>% group_by(type) %>% mutate(total_review = n()) %>% 
-    group_by(type, reviewer, total_review) %>% summarize(num_review = n()) %>% 
+d = data
+d = d %>% select(reviewer, type) %>% group_by(reviewer) %>% mutate(total_review = n()) %>% 
+    group_by(reviewer, type, total_review) %>% summarize(num_review = n()) %>% 
     mutate(fraction_review = num_review / total_review)
 
 p = list()
 for(t in unique(d$type)){
     tmp = d[which(d$type == t),]
-    tmp = data.frame(table(tmp$fraction_review), stringsAsFactors =  F)
-    tmp$Var1 = as.numeric(as.character(tmp$Var1))
-    tmp$Freq = cumsum(tmp$Freq) 
-    tmp$Percent = tmp$Freq / max(tmp$Freq)
+    tmp = data.table(table(tmp$fraction_review))
+    names(tmp) = c('fraction_review', 'num_reviewer')
+    tmp$fraction_review = as.numeric(tmp$fraction_review)
+    tmp$fraction_reviewer = tmp$num_reviewer / sum(tmp$num_reviewer)
+    tmp$cum_fraction_reviewer = cumsum(tmp$fraction_reviewer) 
     if (t == '') {
         tmp$type = 'NA'
     } else {
@@ -167,38 +226,25 @@ for(t in unique(d$type)){
 }
 p = do.call(rbind, p)
 
-# basic CDF
-ggplot() +
-    geom_line(data = p, aes(x = Var1, y = Freq, group = type, col = type)) + 
+
+# basic Cumulative distribution
+ggplot(data = p, aes(x = fraction_review, y = cum_fraction_reviewer, group = type)) +
+    geom_line(aes(col = type, linetype = type)) + 
+    scale_linetype_manual(values = rep(c('solid', 'dashed'), 5)) + 
+    scale_color_manual(values = sort(rep(c(1:4,'grey50'), 2))) + 
     scale_x_continuous('fraction of review') + 
-    scale_y_continuous('number of reviewer')+
-    ggtitle("CDF of Reviewer in given types") +
-    theme(plot.title = element_text(hjust = 0.5))
+    scale_y_continuous('fraction of reviewer') +  
+    ggtitle("Cummulative Reviewer Distribution in given types") 
 ggsave(file="plot/reviewer_type.png")
 
-# basic CDF in log
-ggplot() +
-    geom_line(data = p, aes(x = Var1, y = Freq, group = type, col = type)) + 
-    scale_x_log10('fraction of review') + 
-    scale_y_log10('number of reviewer')+
-    ggtitle("CDF of Reviewer in given types (log)") +
-    theme(plot.title = element_text(hjust = 0.5))
-ggsave(file="plot/reviewer_type_log.png")
+# basic Cumulative distribution logy
+ggplot(data = p, aes(x = fraction_review, y = cum_fraction_reviewer, group = type)) +
+    geom_line(aes(col = type, linetype = type)) + 
+    scale_linetype_manual(values = rep(c('solid', 'dashed'), 5)) + 
+    scale_color_manual(values = sort(rep(c(1:4,'grey50'), 2))) + 
+    scale_x_continuous('fraction of review') + 
+    scale_y_log10('fraction of reviewer') +  
+    ggtitle("Cummulative Reviewer Distribution given Types (logy)") 
+ggsave(file="plot/reviewer_type_logy.png")
 
-# # Normalized CDF
-# ggplot() +
-#     geom_line(data = p, aes(x = Var1, y = Percent, group = type, col = type)) + 
-#     scale_x_continuous('fraction of review') + 
-#     scale_y_continuous('percentage') + 
-#     ggtitle("Reviewer in given types (normalized)") +
-#     theme(plot.title = element_text(hjust = 0.5))
-# ggsave(file="plot/reviewer_type_percent.png")
-# 
-# # Normalized CDF in log
-# ggplot() +
-#     geom_line(data = p, aes(x = Var1, y = Percent, group = type, col = type)) + 
-#     scale_x_log10('fraction of review') + 
-#     scale_y_continuous('percentage') +
-#     ggtitle("Reviewer in given types (normalized, log)") +
-#     theme(plot.title = element_text(hjust = 0.5))
-# ggsave(file="plot/reviewer_type_percent_log.png")
+
