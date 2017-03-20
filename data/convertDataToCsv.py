733d6872c8e9f93b537b8f1e710fb199b4c5374b
陳柏亨 <PoHeng.Chen@gmail.com>
2017-03-19 20:57:32 -0400

    1. revised orgData part to extract description of organizations

--- data/convertDataToCsv.py
+++ data/convertDataToCsv.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 ###### organization data
 # read in all organization data
-dir_base = 'data/orgData/'
+dir_base = 'orgData/'
 files = os.listdir(dir_base)
 
 org_data = pd.DataFrame()
@@ -15,18 +15,20 @@ for file_dir in files:
     state = file_dir[:-4]
     d = [row.replace('\n','').split('\t') for row in file]
     d = pd.DataFrame(d[1: ], columns = d[0])
+    # to combine multiple columns 
+    d['description'] = d[d.columns[9:]].apply(lambda x: ' '.join(x), axis=1)
     d['state'] = state
-    d = d[['name', 'url', 'telephone', 'tax_num', 'street', 'locality', 'state', 'region', 'postcode', 'country']]
+    d = d[['name', 'url', 'telephone', 'tax_num', 'street', 'locality', 'state', 'region', 'postcode', 'country', 'description']]
     org_data = org_data.append(d, ignore_index=True)
     print '%s is done.' % state
 
-org_data.to_csv('data/orgData.csv', index = False)
+org_data.to_csv('orgData.csv', index = False)
 
 
 
 ###### organization reviews
 # read in all organization reviews
-dir_base = 'data/orgReview/'
+dir_base = 'orgReview/'
 files = os.listdir(dir_base)
 
 org_review = pd.DataFrame()
@@ -41,4 +43,4 @@ for file_dir in files:
     org_review = org_review.append(d, ignore_index=True)
     print '%s is done.' % state
 
-org_review.to_csv('data/orgReview.csv', index = False)
+org_review.to_csv('orgReview.csv', index = False)
